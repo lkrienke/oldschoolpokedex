678dbf44b4dd709dc9df996b05d4053f2997aa2b
Laurel <laurel@getflow.com>
2017-05-28 11:54:03 -0700

    Initial commit

---
+++ app/src/main/java/com/jazzyapps/oldschoolpokedex/ui/activities/ActMain.java
@@ -0,0 +1,15 @@
+package com.jazzyapps.oldschoolpokedex.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jazzyapps.oldschoolpokedex.R;
+
+public class ActMain extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.act_main);
+  }
+}
